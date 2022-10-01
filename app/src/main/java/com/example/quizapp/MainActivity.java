42733cc73efe65cf20ef9305225a9aed812aeef2
kendrick chukwuka <kendrickchibueze@gmail.com>
2022-10-01 16:30:17 +0100

    Added more layout files

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -1,24 +1,21 @@
 package com.example.quizapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private int correctAnswers = 0;
+    private int correctAnswers;
     private CheckBox qOneAnswerOne;
     private CheckBox qOneAnswerTwo;
     private CheckBox qOneAnswerThree;
